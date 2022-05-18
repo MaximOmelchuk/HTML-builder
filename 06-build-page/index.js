00924fc265852c8b9a72c89a46d1b02cbc26ac1d
Maxim Omelchuk <omelchukmaxim@gmail.com>
2022-05-18 17:35:12 +0300

    feat: done 6/6 1.3

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -5,32 +5,24 @@ const callback = (err) => {
 };
 // REPLACE TAGS
 fs.rm(path.join(__dirname, 'project-dist'), {recursive: true, force: true}, () => {
-  let copyArr = [];
-  fs.mkdir(path.join(__dirname, 'project-dist'), {recursive: true}, callback);
-  fs.readFile(path.join(__dirname, 'template.html'), (err, data) => {
-    let arr = data.toString().split('\n');
-    fs.rm(path.join(__dirname, 'project-dist', 'index.html'), {force: true, recursive: true}, callback);
-    const insertFunc = (title, x, i) => {
-      fs.readFile(path.join(__dirname, 'components', `${title}.html`), (err, data) => {
-        let prefix = x.replace(/{{.*/, '').slice(0, -1);
-        data = data.toString().split('\n').map(x => prefix + x).join('');
-        copyArr[i] = data;
-        if (title === 'footer') {
-          fs.appendFile(path.join(__dirname, 'project-dist', 'index.html'), copyArr.join('\n'), callback);
-        }
-      });
-    };
-
-    arr.forEach((x, i) => {
-      if (x.includes('articles')) {
-        insertFunc('articles', x, i);
-      } else if (x.includes('footer')) {
-        insertFunc('footer', x, i);
-      } else if (x.includes('header')) {
-        insertFunc('header', x, i);
-      } else copyArr[i] = x;
-    });
-  });
+  fs.readdir(path.join(__dirname, 'components'), (err, components) => {
+    fs.readFile(path.join(__dirname, 'template.html'), (err, data) => {
+        template = data.toString();
+        fs.rm(path.join(__dirname, 'project-dist', 'index.html'), {force: true, recursive: true}, callback);
+        componentsNames = components.map(item => item.split('.')[0]);
+        componentsNames.forEach((x,i) => {
+            fs.readFile(path.join(__dirname, 'components', components[i]), (err, component) => {
+                // let re = new RegExp(`{{${x}}}.*`);
+                // let prefix = x.replace(re, '').slice(0, -1);
+                // data = data.toString().split('\n').map(x => prefix + x).join('');
+                template = template.replace(`{{${x}}}`, component);
+                if (i === components.length-1) {
+                    fs.appendFile(path.join(__dirname, 'project-dist', 'index.html'), template, callback);
+                }
+            });
+        })
+    })
+  })
   // MERGE STYLES
   fs.rm(path.join(__dirname, 'project-dist', 'style.css'), () => {
     fs.readdir(path.join(__dirname, 'styles'), (err, arr) => {
@@ -71,4 +63,29 @@ fs.rm(path.join(__dirname, 'project-dist'), {recursive: true, force: true}, () =
   }); 
 });
 
-  
+// let copyArr = [];
+//   fs.mkdir(path.join(__dirname, 'project-dist'), {recursive: true}, callback);
+//   fs.readFile(path.join(__dirname, 'template.html'), (err, data) => {
+//     let arr = data.toString().split('\n');
+//     fs.rm(path.join(__dirname, 'project-dist', 'index.html'), {force: true, recursive: true}, callback);
+//     const insertFunc = (title, x, i) => {
+//       fs.readFile(path.join(__dirname, 'components', `${title}.html`), (err, data) => {
+//         let prefix = x.replace(/{{.*/, '').slice(0, -1);
+//         data = data.toString().split('\n').map(x => prefix + x).join('');
+//         copyArr[i] = data;
+//         if (title === 'footer') {
+//           fs.appendFile(path.join(__dirname, 'project-dist', 'index.html'), copyArr.join('\n'), callback);
+//         }
+//       });
+//     };
+
+//     arr.forEach((x, i) => {
+//       if (x.includes('articles')) {
+//         insertFunc('articles', x, i);
+//       } else if (x.includes('footer')) {
+//         insertFunc('footer', x, i);
+//       } else if (x.includes('header')) {
+//         insertFunc('header', x, i);
+//       } else copyArr[i] = x;
+//     });
+//   });  
